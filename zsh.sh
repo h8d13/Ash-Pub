83335288c6cd22c967ad565c6d7f0101ecf91faf
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-17 15:06:03 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -8,10 +8,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/plugins/z
 git clone https://github.com/zsh-users/zsh-syntax-highlighting "$HOME/.zsh/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-history-substring-search "$HOME/.zsh/plugins/zsh-history-substring-search"
 
-# === Install fzf ===
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --key-bindings --completion --no-update-rc
-
 # === Create ~/.config/zsh directory if it doesn't exist ===
 mkdir -p "$HOME/.config/zsh"
 
@@ -21,15 +17,6 @@ cat > "$HOME/.config/zsh/zshrc" << 'EOF'
 source "$HOME/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "$HOME/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 source "$HOME/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh"
-
-# === zsh-history-substring-search key bindings (Up/Down) ===
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-
-# Required for proper cursor movement
-autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
-zle -N history-substring-search-up
-zle -N history-substring-search-down
 
 # === Custom Prompt ===
 export PROMPT='%F{blue}┌──[%F{cyan}%*%F{blue}]─[%F{default}%n%F{blue}@%F{cyan}%m%F{blue}]─[%F{green}%~%F{blue}]
