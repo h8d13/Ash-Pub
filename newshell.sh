e0e70e003b5ad3449f191f7bc7484ef454f6888d
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-17 12:40:01 +0200

    Update newshell.sh

--- newshell.sh
+++ newshell.sh
@@ -1,39 +1,43 @@
 #!/bin/sh
-
-# Exit on any error
 set -e
 
-# Ensure running as root
-if [ "$(id -u)" -ne 0 ]; then
-  echo "Please run as root or with sudo"
+# === CONFIG ===
+# Detect user: fallback to root if unknown
+if [ "$SUDO_USER" ]; then
+  USER_NAME="$SUDO_USER"
+elif [ "$USER" != "root" ]; then
+  USER_NAME="$USER"
+else
+  echo "❗ Unable to detect non-root user. Run this with sudo from a regular user."
   exit 1
 fi
 
-USER_NAME="$(logname)"
 USER_HOME="/home/$USER_NAME"
+ZSHRC="$USER_HOME/.zshrc"
 
-# Install necessary packages
+echo "Installing for user: $USER_NAME"
+
+# === INSTALL DEPENDENCIES ===
 apk update
 apk add zsh git curl wget unzip fontconfig
 
-# Install Oh My Zsh
+# === INSTALL OH MY ZSH ===
 sudo -u "$USER_NAME" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Install Powerlevel10k theme
+# === INSTALL POWERLEVEL10K ===
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k"
 
-# Set ZSH_THEME in .zshrc
-ZSHRC="$USER_HOME/.zshrc"
+# Update .zshrc to use Powerlevel10k
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$ZSHRC"
 
-# Install plugins
+# === INSTALL OPTIONAL PLUGINS ===
 git clone https://github.com/zsh-users/zsh-autosuggestions "$USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting "$USER_HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 
-# Enable plugins in .zshrc
+# Enable them in .zshrc
 sed -i 's/^plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' "$ZSHRC"
 
-# Set up font (JetBrainsMono Nerd Font)
+# === INSTALL NERD FONTS ===
 FONT_DIR="/usr/share/fonts/nerd-fonts"
 mkdir -p "$FONT_DIR"
 TMP_ZIP="/tmp/JetBrainsMono.zip"
@@ -42,9 +46,8 @@ unzip -o "$TMP_ZIP" -d "$FONT_DIR"
 fc-cache -fv
 rm "$TMP_ZIP"
 
-# Set ownership
+# === SET PERMISSIONS ===
 chown -R "$USER_NAME:$USER_NAME" "$USER_HOME/.oh-my-zsh"
 chown "$USER_NAME:$USER_NAME" "$ZSHRC"
 
-echo "Zsh with Oh My Zsh + Powerlevel10k + Nerd Font installed!"
-echo "Run 'zsh' to enjoy!"
+echo "Setup complete!"
