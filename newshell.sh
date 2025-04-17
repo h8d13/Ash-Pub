1bb889e0c2950fcea06e6a9e89f67f021c57e6d4
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-17 12:45:55 +0200

    Update newshell.sh

--- newshell.sh
+++ newshell.sh
@@ -1,34 +1,23 @@
 #!/bin/sh
 set -e
 
-# === User and paths ===
-TARGET_USER="$USER"
-USER_HOME="/home/$TARGET_USER"
-ZSHRC="$USER_HOME/.zshrc"
-
-if [ ! -d "$USER_HOME" ]; then
-  echo "Home directory for user '$TARGET_USER' not found at $USER_HOME"
-  echo "If you're running this as root for another user, set TARGET_USER manually."
-  exit 1
-fi
-
 # === Install required packages ===
 apk update
 apk add zsh git curl wget unzip fontconfig
 
 # === Install Oh My Zsh ===
-su - "$TARGET_USER" -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # === Install Powerlevel10k ===
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k"
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$ZSHRC"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 
 # === Install plugins ===
-git clone https://github.com/zsh-users/zsh-autosuggestions "$USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting "$USER_HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
-sed -i 's/^plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' "$ZSHRC"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sed -i 's/^plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
-# === Install JetBrainsMono Nerd Font ===
+# === Install Nerd Font (JetBrainsMono) ===
 FONT_DIR="/usr/share/fonts/nerd-fonts"
 mkdir -p "$FONT_DIR"
 TMP_ZIP="/tmp/JetBrainsMono.zip"
@@ -37,8 +26,4 @@ unzip -o "$TMP_ZIP" -d "$FONT_DIR"
 fc-cache -fv
 rm "$TMP_ZIP"
 
-# === Permissions ===
-chown -R "$TARGET_USER:$TARGET_USER" "$USER_HOME/.oh-my-zsh"
-chown "$TARGET_USER:$TARGET_USER" "$ZSHRC"
-
-echo "Zsh with Oh My Zsh, Powerlevel10k, and plugins installed for user: $TARGET_USER"
+echo "Zsh + Oh My Zsh + Powerlevel10k installed."
