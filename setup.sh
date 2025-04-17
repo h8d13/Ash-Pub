935939d5edd14f645af388da7800406b3997e463
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-17 13:20:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,26 +7,20 @@ apk update
 ## Extended ascii support for later :)
 apk add --no-cache tzdata font-noto-emoji fontconfig musl-locales
 
-# Create /etc/profile.d/profile.sh to source user profile if it exists
+# Create /etc/profile.d/profile.sh to source user profile if it exists & Make exec
 cat > /etc/profile.d/profile.sh << 'EOF'
 if [ -f "$HOME/.config/ash/profile" ]; then
     . "$HOME/.config/ash/profile"
 fi
 EOF
 
-# Make it executable
 chmod +x /etc/profile.d/profile.sh
-echo "Created and made /etc/profile.d/profile.sh executable."
-
 # Create ~/.config/ash directory if it doesn't exist
 mkdir -p "$HOME/.config/ash"
-
-# Create ~/.config/ash/profile
+# Create ~/.config/ash/profile and add basic style 
 echo 'export ENV="$HOME/.config/ash/ashrc"' > "$HOME/.config/ash/profile"
-echo "Created $HOME/.config/ash/profile."
 
 cat > "$HOME/.config/ash/ashrc" << 'EOF'
-# Custom prompt with time display in blue color scheme
 export PS1='\033[0;34m┌──[\033[0;36m\t\033[0;34m]─[\033[0;39m\u\033[0;34m@\033[0;36m\h\033[0;34m]─[\033[0;32m\w\033[0;34m]\n\033[0;34m└──╼ \033[0;36m$ \033[0m'
 EOF
 
@@ -38,7 +32,7 @@ Set up the system with: setup-alpine
 Apk sources /etc/apk/repositories
 Change this message by editing /etc/motd
 Change the pre-login message /etc/issue
-Made with <3 by H8D13. 
+Custom with <3 by H8D13. 
 EOF
 
 cat > /etc/issue << 'EOF'
