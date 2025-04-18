5c8294e598524e72e08bfd7e64a55b9366aa438b
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-18 20:24:52 +0200

    fix icons test: pass #0001

--- utils/tests/fixicons.sh
+++ utils/tests/fixicons.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 TARGET_USER=hill
 sed -i '/\[Containments\]\[2\]\[Applets\]\[5\]\[Configuration\]\[General\]/,/^\[/ s/launchers=.*/launchers=applications:org.kde.konsole.desktop/' "/home/$TARGET_USER/.config/plasma-org.kde.plasma.desktop-appletsrc"
