ff651cbc6d6b478b059d38a6fb868ca872ac4350
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-18 13:04:05 +0200

    Create kdetests.sh

---
+++ utils/tests/kdetests.sh
@@ -0,0 +1,4 @@
+# Some things i'd like to do
+# Modify default theme to dark
+# Modify icons in taskbar to be empty (feel like a fresh install) 
+# Modify default Konsole profile to use su -l directly. 
