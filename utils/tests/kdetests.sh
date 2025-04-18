7abedd766bb0b68439275fd34b3fc3fa6ee73323
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-18 15:17:27 +0200

    Update kdetests.sh

--- utils/tests/kdetests.sh
+++ utils/tests/kdetests.sh
@@ -2,5 +2,16 @@
 # Modify default theme to dark
 # Modify icons in taskbar to be empty (feel like a fresh install) 
 # Modify default Konsole profile to use su -l directly. 
-
 # Modify default bg & start icon 
+
+plasma-apply-desktoptheme breeze-dark
+plasma-apply-colorscheme BreezeDark
+plasma-apply-lookandfeel --apply org.kde.breezedark.desktop
+
+#copy personal image to 
+#> /home/$TARGET_USER/Pictures 
+#same for a custom icon for start menu
+
+# Remove icons # 
+
+#/usr/share/applications/kde-mimeapps.list 
