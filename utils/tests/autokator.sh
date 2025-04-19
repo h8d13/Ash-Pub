daa7ddefb9d4b2948a994831d7c6a579a50df4e6
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-19 09:32:18 +0200

    pass pass #0002

--- utils/tests/autokator.sh
+++ utils/tests/autokator.sh
@@ -7,5 +7,6 @@ KB_LAYOUT=$(ls /etc/keymap/*.bmap.gz 2>/dev/null | head -1 | sed 's|/etc/keymap/
 
 # Print the result
 echo "TARGET_USER=$TARGET_USER"
+echo "KB_LAYOUT=$KB_LAYOUT"
 
-# Pass :)
+# Pass & pass :)
