278de3ec49ce40ff9c8dd449b0ce75e36c116299
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-19 09:29:32 +0200

    Update autokator.sh

--- utils/tests/autokator.sh
+++ utils/tests/autokator.sh
@@ -2,6 +2,9 @@
 
 ## Hacky way to get the username auto
 TARGET_USER=$(cat /etc/passwd | grep '/home/' | head -1 | cut -d: -f1)
+
+KB_LAYOUT=$(ls /etc/keymap/*.bmap.gz 2>/dev/null | head -1 | sed 's|/etc/keymap/||' | sed 's|\.bmap\.gz$||')
+
 # Print the result
 echo "TARGET_USER=$TARGET_USER"
 
