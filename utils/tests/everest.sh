af547a8d0c2e3596cce00df59a7a8c2fd267dd53
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-21 15:04:19 +0200

    fail test 2 test #0003

--- utils/tests/everest.sh
+++ utils/tests/everest.sh
@@ -2,32 +2,31 @@
 # Script for dual boot Arch & Alpine
 set -e  # Exit on error
 
-# WHAT DO WE NEED IN ALPINE ?
+# WHAT DO WE NEED IN ALPINE?
 echo "Installing required packages in Alpine..."
-apk add wget curl zstd dosfstools arch-install-scripts
+apk add wget curl zstd dosfstools arch-install-scripts parted efibootmgr
 
-## Make sure user doesnt have a file named arch-bootstrap in /tmp
-## Case: failed previous install
+# Clean up previous attempt files
 rm -rf /tmp/archlinux-bootstrap*
 
-# Configuration variables - adjust these
-TARGET_DISK="/dev/sdb"  # The disk to install Arch on
-# Find out where the Alpine install is (so we don't overwrite it.) Currently manual
-# Try lazy unmount
+# Configuration variables
+TARGET_DISK="/dev/sdb"
+TARGET_HOSTNAME="archlinux"
+TARGET_TIMEZONE="Europe/Paris"
+ROOT_PASSWORD="Everest"
+TARGET_MOUNT="/mnt/arch"
+
+# Create mount directory
+mkdir -p "$TARGET_MOUNT"
+
+# Ensure all partitions are unmounted
+echo "Ensuring all partitions are unmounted..."
 umount -l "$TARGET_MOUNT" 2>/dev/null || true
 umount -l "${TARGET_DISK}1" 2>/dev/null || true
 umount -l "${TARGET_DISK}2" 2>/dev/null || true
+umount -fl "${TARGET_DISK}*" 2>/dev/null || true
 
-TARGET_HOSTNAME="archlinux"
-## Use the same hostname as Alpine install + arch
-TARGET_TIMEZONE="Europe/Paris"
-ROOT_PASSWORD="Everest"  # Please change this!
-TARGET_MOUNT="/mnt/arch"
-
-# Create directories
-mkdir -p "$TARGET_MOUNT"
-
-# Partitioning - CAUTION: This will erase all data on TARGET_DISK
+# Partitioning
 echo "Partitioning $TARGET_DISK..."
 parted -s "$TARGET_DISK" mklabel gpt
 parted -s "$TARGET_DISK" mkpart primary fat32 1MiB 513MiB
@@ -49,7 +48,6 @@ mount "${TARGET_DISK}1" "$TARGET_MOUNT/boot/efi"
 echo "Downloading Arch Linux bootstrap..."
 cd /tmp
 wget https://mirrors.edge.kernel.org/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst
-
 zstd -d archlinux-bootstrap-x86_64.tar.zst
 tar -xf archlinux-bootstrap-x86_64.tar -C /tmp
 cp -a /tmp/root.x86_64/* "$TARGET_MOUNT"/
@@ -65,7 +63,18 @@ mount -t sysfs /sys "$TARGET_MOUNT/sys"
 mount -o bind /dev "$TARGET_MOUNT/dev"
 mount -o bind /dev/pts "$TARGET_MOUNT/dev/pts"
 cp /etc/resolv.conf "$TARGET_MOUNT/etc/"
-echo "Keyrings"
+
+# Check for EFI support and mount if available
+if [ -d /sys/firmware/efi/efivars ]; then
+  echo "EFI system detected, mounting EFI variables..."
+  mkdir -p "$TARGET_MOUNT/sys/firmware/efi/efivars"
+  mount -o bind /sys/firmware/efi/efivars "$TARGET_MOUNT/sys/firmware/efi/efivars" || echo "Warning: Could not mount EFI variables"
+else
+  echo "BIOS system detected, will configure GRUB accordingly"
+fi
+
+# Initialize pacman keyring
+echo "Initializing pacman keyring..."
 chroot "$TARGET_MOUNT" /bin/bash -c "pacman-key --init && pacman-key --populate archlinux"
 
 # Bootstrap the system
@@ -88,9 +97,20 @@ echo "::1 localhost" >> /etc/hosts
 echo "127.0.1.1 $TARGET_HOSTNAME.localdomain $TARGET_HOSTNAME" >> /etc/hosts
 echo "root:$ROOT_PASSWORD" | chpasswd
 pacman -S --noconfirm grub efibootmgr networkmanager base-devel sudo os-prober
+
 echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
 systemctl enable NetworkManager
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+
+# Install GRUB based on system type
+if [ -d /sys/firmware/efi/efivars ]; then
+  echo "Installing GRUB for UEFI system..."
+  grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
+else
+  echo "Installing GRUB for BIOS system..."
+  grub-install --target=i386-pc $TARGET_DISK
+fi
+
+# Generate GRUB config
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
@@ -99,5 +119,5 @@ chroot "$TARGET_MOUNT" /configure.sh
 
 # Cleanup
 echo "Cleaning up..."
-umount -R "$TARGET_MOUNT"
+umount -R "$TARGET_MOUNT" || echo "Warning: Some filesystems could not be unmounted, may need manual cleanup"
 echo "Arch Linux installation complete!"
