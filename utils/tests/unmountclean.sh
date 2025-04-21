633f30814b16fa8fc9dc1362aa1b655d9905b268
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-21 12:22:42 +0200

    Update unmountclean.sh

--- utils/tests/unmountclean.sh
+++ utils/tests/unmountclean.sh
@@ -3,3 +3,11 @@ for i in $(mount | grep "^${TARGET_DISK}" | cut -d' ' -f1); do
   echo "Unmounting $i..."
   umount -f "$i" || true
 done
+
+fuser -km "$TARGET_MOUNT" 2>/dev/null || true
+
+
+# Try lazy unmount
+umount -l "$TARGET_MOUNT" 2>/dev/null || true
+umount -l "${TARGET_DISK}1" 2>/dev/null || true
+umount -l "${TARGET_DISK}2" 2>/dev/null || true
