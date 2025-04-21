ad2d978d549bf38fd8d49727b1c9b55331d5bf41
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-21 16:48:18 +0200

    #2104 create prober

---
+++ utils/bobw/probe.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# REQ
+apk add os-prober
+
+# Ensure os-prober is enabled in GRUB config
+if ! grep -q "GRUB_DISABLE_OS_PROBER=false" /etc/default/grub; then
+    echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+fi
+
+# Make sure the USB drive is connected and mounted
+# You don't need to keep it mounted, just mount it so os-prober can detect it
+mkdir -p /mnt/usb
+mount /dev/sdb2 /mnt/usb
+mount /dev/sdb1 /mnt/usb/boot ## Usually part 1 is boot
+
+# Update GRUB config on the host
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Unmount / Cleanup
+umount -R /mnt/usb
