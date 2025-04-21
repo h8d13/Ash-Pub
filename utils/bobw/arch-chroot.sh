0858ab97163f2ef7fe5788b9bf0c4f48992e48b8
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-21 19:56:43 +0200

    #2104 test #0001 pass + removed cp

--- utils/bobw/arch-chroot.sh
+++ utils/bobw/arch-chroot.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-### For quick maintenance from within alpine.
+### For quick maintenance from within alpine. 
 
 # Create mount point if it doesn't exist
 mkdir -p /mnt/arch
 
 # Mount Arch partitions
 mount /dev/sdb3 /mnt/arch
+#2 is usually swap
 mount /dev/sdb1 /mnt/arch/boot
 
 # Mount virtual filesystems
@@ -13,9 +14,6 @@ mount -t proc /proc /mnt/arch/proc
 mount -t sysfs /sys /mnt/arch/sys
 mount -o bind /dev /mnt/arch/dev
 mount -o bind /dev/pts /mnt/arch/dev/pts
-
-# Copy resolv.conf for network access
-cp /etc/resolv.conf /mnt/arch/etc/resolv.conf
 
 # Chroot into Arch
 echo "Entering Arch Linux chroot environment..."
@@ -30,4 +28,5 @@ umount /mnt/arch/proc
 umount /mnt/arch/boot
 umount /mnt/arch
 
-echo "Chroot session ended."
+echo "Chroot session ended. Unmouted filesystems."
+
