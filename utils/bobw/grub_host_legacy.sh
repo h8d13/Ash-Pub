c5eaf4f63b9809519bf16c6c66ee6944669f601b
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-22 12:52:49 +0200

    #2501 restrucs

---
+++ utils/bobw/grub_host_legacy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Install os-prober if needed
+apk add os-prober grub-bios
+
+# Enable os-prober in GRUB config
+cat > /etc/default/grub << EOF
+GRUB_TIMEOUT=10
+GRUB_TIMEOUT_STYLE=menu
+GRUB_DISABLE_OS_PROBER=false
+EOF
+
+# Weird is that legacy is much easier to detect kind of just works by itself. 
+# Update GRUB configuration (this runs os-prober automatically)
+grub-mkconfig -o /boot/grub/grub.cfg
