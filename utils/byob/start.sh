9a7f2043b2992f916ac3a3cab5957a3795d4a253
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-23 19:47:25 +0200

    Update start.sh

--- utils/byob/start.sh
+++ utils/byob/start.sh
@@ -1,1 +1,1 @@
-# ./mkimage.sh --tag edge --outdir ~/out --arch x86_64 --repository https://dl-cdn.alpinelinux.org/alpine/edge/main --profile k2alpine --hostkeys
+# ./mkimage.sh --tag edge --outdir ~/out --arch x86_64 --repository https://dl-cdn.alpinelinux.org/alpine/edge/main --profile kalpine --hostkeys
