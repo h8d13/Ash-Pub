a7af1bcb6bd069827edf475ee3484c6a8b4f1989
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-23 17:33:56 +0200

    Update and rename setup-alpine-wrapper.sh to genmkimage.sh

---
+++ utils/byob/genmkimage.sh
@@ -0,0 +1,23 @@
+export PROFILENAME=k2alpine
+
+# Create profile script
+cat << EOF > ~/aports/scripts/mkimg.$PROFILENAME.sh
+profile_$PROFILENAME() {
+    profile_standard
+    kernel_cmdline="unionfs_size=512M console=tty0 console=ttyS0,115200"
+    syslinux_serial="0 115200"
+    apks="\$apks git"
+    local _k _a
+    for _k in \$kernel_flavors; do
+        apks="\$apks linux-\$_k"
+        for _a in \$kernel_addons; do
+            apks="\$apks \$_a-\$_k"
+        done
+    done
+    apks="\$apks linux-firmware"
+    apkovl="genapkovl-k2alpine.sh"
+}
+EOF
+
+# Make it executable
+chmod +x ~/aports/scripts/mkimg.$PROFILENAME.sh
