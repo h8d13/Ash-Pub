8570974c74b0b98fec8a0935bc8f688504b556fa
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-23 17:42:54 +0200

    Update genmkimage.sh

--- utils/byob/genmkimage.sh
+++ utils/byob/genmkimage.sh
@@ -1,23 +1,15 @@
-export PROFILENAME=k2alpine
-
-# Create profile script
-cat << EOF > ~/aports/scripts/mkimg.$PROFILENAME.sh
-profile_$PROFILENAME() {
+profile_k2alpine() {
     profile_standard
     kernel_cmdline="unionfs_size=512M console=tty0 console=ttyS0,115200"
     syslinux_serial="0 115200"
-    apks="\$apks git"
+    apks="$apks git"
     local _k _a
-    for _k in \$kernel_flavors; do
-        apks="\$apks linux-\$_k"
-        for _a in \$kernel_addons; do
-            apks="\$apks \$_a-\$_k"
+    for _k in $kernel_flavors; do
+        apks="$apks linux-$_k"
+        for _a in $kernel_addons; do
+            apks="$apks $_a-$_k"
         done
     done
-    apks="\$apks linux-firmware"
+    apks="$apks linux-firmware"
     apkovl="genapkovl-k2alpine.sh"
 }
-EOF
-
-# Make it executable
-chmod +x ~/aports/scripts/mkimg.$PROFILENAME.sh
