e7dfbab2d0443001add8a85e80e41fca47ba8deb
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-23 19:15:09 +0200

    ## eazepaz

--- utils/byob/mkimg.k2akpine.sh
+++ utils/byob/mkimg.k2akpine.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
+#export PROFILENAME=k2alpine
 #/scripts/mkimg.k2akpine.sh
-profile_k2alpine() {
-    profile_standard
-    kernel_cmdline="unionfs_size=512M console=tty0 console=ttyS0,115200"
-    syslinux_serial="0 115200"
-    apks="$apks git"
-    local _k _a
-    for _k in $kernel_flavors; do
-        apks="$apks linux-$_k"
-        for _a in $kernel_addons; do
-            apks="$apks $_a-$_k"
+
+cat << EOF > ~/aports/scripts/mkimg.$PROFILENAME.sh
+profile_$PROFILENAME() {
+        profile_standard
+        kernel_cmdline="unionfs_size=512M console=ttyS0,115200"
+        syslinux_serial="0 115200"
+        apks="\$apks alpine-base git"
+        
+        local _k _a
+        for _k in \$kernel_flavors; do
+                apks="\$apks linux-\$_k"
+                for _a in \$kernel_addons; do
+                        apks="\$apks \$_a-\$_k"
+                done
         done
-    done
-    apks="$apks linux-firmware"
-    apkovl="genapkovl-k2alpine.sh"
+        apks="\$apks linux-firmware"
+        
+        # Include our custom overlay setup
+        apkovl="aports/scripts/genapkovl-k2alpine.sh"
 }
+EOF
+
+chmod +x ~/aports/scripts/mkimg.$PROFILENAME.sh
 
 
 # ./mkimage.sh --profile k2alpine --outdir ~/out --repository https://dl-cdn.alpinelinux.org/latest-stable/main --arch x86_64 --hostkey
