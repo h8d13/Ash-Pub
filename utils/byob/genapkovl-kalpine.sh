15f37c8a806f36b41b19808cd240059fa8b852ea
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-24 12:24:37 +0200

    final edits ##

--- utils/byob/genapkovl-kalpine.sh
+++ utils/byob/genapkovl-kalpine.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -e
-
-HOSTNAME="$1"
-if [ -z "$HOSTNAME" ]; then
-	echo "usage: $0 hostname"
-	exit 1
-fi
+#/home/build/aports/scripts/genapkovl-kalpine.sh
+HOSTNAME="kalpine"
 
 cleanup() {
 	rm -rf "$tmp"
@@ -25,40 +21,70 @@ rc_add() {
 }
 
 tmp="$(mktemp -d)"
-trap cleanup EXIT
-
+trap cleanup exit
 mkdir -p "$tmp"/etc
 makefile root:root 0644 "$tmp"/etc/hostname <<EOF
 $HOSTNAME
 EOF
-
-mkdir -p "$tmp"/etc/network
-makefile root:root 0644 "$tmp"/etc/network/interfaces <<EOF
-auto lo
-iface lo inet loopback
-
-auto eth0
-iface eth0 inet dhcp
-EOF
-
+## Remove this network part so that the user is prompted to config.
 mkdir -p "$tmp"/etc/apk
 makefile root:root 0644 "$tmp"/etc/apk/world <<EOF
 alpine-base
+git
 EOF
+mkdir -p "$tmp"/etc/local.d
+makefile root:root 0644 "$tmp"/etc/local.d/k2-bc.start <<'EOF'
+#!/bin/sh
+touch /etc/boot_c
+count_file="/etc/boot_c"
+if [ ! -f "$count_file" ]; then
+  echo -1 > "$count_file"
+fi
+BC=$(<"$count_file")
+BC=$((BC+1))
+echo $BC > "$count_file"
+echo "Boot count: $BC - $(date)"
+EOF
+chmod +x "$tmp"/etc/local.d/k2-bc.start
 
+## K2 Setup pre-config # Folder already exists
+makefile root:root 0644 "$tmp"/etc/setup-k2 <<'EOF'
+#!/bin/sh
+echo "Setting up K2 for Alpine Linux 3.21..."
+apk add --no-cache git 
+echo "Cloning then move..."
+git clone https://github.com/h8d13/k2-alpine && cd k2-alpine
+echo "Making exec..."
+chmod +x setup.sh
+echo "Ready. Set. Go!"
+./setup.sh
+cd ..
+echo "Cleaning up."
+rm -rf k2-alpine
+echo "K2 setup complete!"
+EOF
+chmod +x "$tmp"/etc/setup-k2
+## motd for installing
+makefile root:root 0644 "$tmp"/etc/motd <<EOF
+Welcome to K2_OS!
+Use setup-alpine. Then reboot to hardisk.
+After reboot:
+". /etc/setup-k2"
+EOF
+## init/boot/shutdown/default
+rc_add local default
 rc_add devfs sysinit
 rc_add dmesg sysinit
 rc_add mdev sysinit
 rc_add hwdrivers sysinit
 rc_add modloop sysinit
-
 rc_add hwclock boot
 rc_add modules boot
 rc_add sysctl boot
 rc_add hostname boot
 rc_add bootmisc boot
+rc_add networking boot
 rc_add syslog boot
-
 rc_add mount-ro shutdown
 rc_add killprocs shutdown
 rc_add savecache shutdown
