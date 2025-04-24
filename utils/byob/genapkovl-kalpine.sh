faa24c7cadb8f39cade44985cdbdead3f1db5868
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-24 14:18:55 +0200

    # mistakes

--- utils/byob/genapkovl-kalpine.sh
+++ utils/byob/genapkovl-kalpine.sh
@@ -36,14 +36,33 @@ mkdir -p "$tmp"/etc/local.d
 makefile root:root 0644 "$tmp"/etc/local.d/k2-bc.start <<'EOF'
 #!/bin/sh
 count_file="/etc/boot_c"
-log_file="/var/log/bc_log"
-touch "$count_file"
-touch "$log_file"
 if [ ! -f "$count_file" ]; then
-  echo -1 > "$count_file"
+  echo "0" > "$count_file"
 fi
 BC=$(<"$count_file")
 BC=$((BC+1))
+echo "$BC" > "$count_file"
+EOF
+chmod +x "$tmp"/etc/local.d/k2-bc.start
+makefile root:root 0644 "$tmp"/etc/local.d/k2-bc-log.start <<'EOF'
+#!/bin/sh
+count_file="/etc/boot_c"
+log_file="/var/log/bc_log"
+touch "$log_file"
+BC=$(<"$count_file")
+echo "BC: $BC - $(date) - ${USER:-system} - PID:$$" >> "$log_file"
+if [ "$BC" = "1" ]; then
+  echo "SYSTEM READY FOR SETUP" >> "$log_file"
+fi
+EOF
+chmod +x "$tmp"/etc/local.d/k2-bc-log.start
+
+makefile root:root 0644 "$tmp"/etc/local.d/k2-bc.start <<'EOF'
+#!/bin/sh
+count_file="/etc/boot_c"
+log_file="/var/log/bc_log"
+touch "$log_file"
+BC=$(<"$count_file")
 echo $BC > "$count_file"
 echo "BC: $BC - $(date) - ${USER:-system} - PID:$$" >> "$log_file"
 if [ "$BC" = "1" ]; then
@@ -51,6 +70,7 @@ if [ "$BC" = "1" ]; then
 fi
 EOF
 chmod +x "$tmp"/etc/local.d/k2-bc.start
+
 ## K2 Setup pre-config # Folder already exists
 makefile root:root 0644 "$tmp"/etc/setup-k2 <<'EOF'
 #!/bin/sh
