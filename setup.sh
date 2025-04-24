3d8673cadc6b8972b82ded4ba44a85d619187d44
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-25 00:54:47 +0200

    # added restart to kpost.sh

--- setup.sh
+++ setup.sh
@@ -154,6 +154,7 @@ plasma-apply-desktoptheme breeze-dark
 plasma-apply-colorscheme BreezeDark
 # Restart Plasma to apply changes
 killall plasmashell && kstart5 plasmashell
+service sddm restart
 EOF
 chmod +x /home/$TARGET_USER/Desktop/k2-os/etc/kpost.sh
 cat > /home/$TARGET_USER/Desktop/k2-os/runme.sh << EOF
