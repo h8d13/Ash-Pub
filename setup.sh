0eee8f4a6bfa5b91bec1a60da6274ccf98930dbe
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-29 09:46:17 +0200

    # correct kpost

--- setup.sh
+++ setup.sh
@@ -165,13 +165,11 @@ kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc --group Containment
 plasma-apply-desktoptheme breeze-dark
 # Set dark theme for window styles
 plasma-apply-colorscheme BreezeDark
-# Reboot
-doas reboot
 EOF
 chmod +x /home/$TARGET_USER/Desktop/k2-os/etc/kpost.sh
 cat > /home/$TARGET_USER/Desktop/k2-os/runme_once.sh << EOF
 #!/bin/sh
-konsole --builtin-profile -e "/home/$TARGET_USER/Desktop/k2-os/etc/kpost.sh"
+konsole --builtin-profile -e "/home/$TARGET_USER/Desktop/k2-os/etc/kpost.sh" && reboot
 EOF
 chmod +x /home/$TARGET_USER/Desktop/k2-os/runme_once.sh
 ######################################### FIX SESSIONS
