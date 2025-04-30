15bc21f1259e4b1710e644615e6641459c1d873b
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-30 11:20:53 +0200

    # hl headers

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 ## /* SPDX-FileCopyrightText: 2025 
 # (O) Eihdran L. <hadean-eon-dev@proton.me>
 # (C) Lagan S. <sarbjitsinghsandhu509@gmail.com>
-# Desc: Extended Install Intel.
+# Desc: Extended Install Intel / Wayland (+Optionals).
 ##  SPDX-License-Identifier: MIT */
 #set -e
 #set -x
