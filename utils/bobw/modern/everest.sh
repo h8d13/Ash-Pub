e3b7dd75a1c275a2dc746f7126f0ab5786c63a9c
HADEON <52324046+h8d13@users.noreply.github.com>
2025-05-02 11:57:37 +0200

    Update everest.sh

--- utils/bobw/modern/everest.sh
+++ utils/bobw/modern/everest.sh
@@ -3,7 +3,6 @@
 #### ALSO GPT / UEFI
 set -e  # Exit on error
 
-
 # Configuration variables
 TARGET_DISK="/dev/sdb" ### VERY CAREFULY LSBLK TO CHECK 
 TARGET_TIMEZONE="Europe/Paris" 
@@ -93,6 +92,38 @@ chmod 440 /etc/sudoers.d/wheel
 
 # Install GRUB and essentials
 pacman -S --noconfirm grub grub-efi-x86_64 networkmanager base-devel sudo util-linux
+pacman -S --noconfirm plasma sddm konsole dolphin
+systemctl enable sddm
+########################################## FIX LOGIN KB
+echo "Setting up Keyboard..." 
+mkdir -p "/usr/share/sddm/scripts/"
+cat >> /usr/share/sddm/scripts/Xsetup << EOF
+setxkbmap "$KB_LAYOUT"
+EOF
+chmod +x /usr/share/sddm/scripts/Xsetup
+########################################## FIX GLOBAL KB
+mkdir -p "/home/$TARGET_USER/.config"
+cat > "/home/$TARGET_USER/.config/kxkbrc" << EOF
+[Layout]
+LayoutList=$KB_LAYOUT
+Use=True
+EOF
+######################################### FIX SESSIONS
+echo "Setting up KDE Config..." 
+## Cool prepend move totally useless file doesnt exist yet but it's cool ya know
+CONFIG_FILE2="/home/$TARGET_USER/.config/ksmserverrc"
+TMP_FILE="$(mktemp)"
+echo -e "[General]\nloginMode=emptySession" > "$TMP_FILE"
+cat "$CONFIG_FILE2" >> "$TMP_FILE" 2>/dev/null # ignore not exist error idk 
+mv "$TMP_FILE" "$CONFIG_FILE2"
+# Basiclally just makes it so that new sessions are fresh (something that I always thought was a stupid default value... 
+# Simple override the whole file for 15 min lockout and 5 min password grace. 
+CONFIG_FILE3="/home/$TARGET_USER/.config/kscreenlockerrc"
+cat <<EOF > $CONFIG_FILE3
+[Daemon]
+LockGrace=300
+Timeout=30
+EOF
 
 # Enable NetworkManager & UFW stuff
 systemctl enable NetworkManager
