dfccdb645ffe107bfadaff72b0ecdbceb38d4f7a
HADEON <52324046+h8d13@users.noreply.github.com>
2025-05-13 05:17:01 +0200

    # alpineception

---
+++ utils/alpineception.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+## script basically creates a mini version of alpine using minimal filesystem (3mb compressed) 
+## magic reset uncomment 
+#rm -rf alpinestein
+## check if the alpinestein directory exists
+if [ ! -d "alpinestein" ]; then
+    mkdir -p alpinestein
+    wget https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-minirootfs-3.21.3-x86_64.tar.gz -O tmp.tar.gz
+    tar xzf tmp.tar.gz -C alpinestein
+    rm tmp.tar.gz
+else
+    echo "The alpinestein directory already exists. Skipping download and extraction."
+fi
+
+## ash dash stuff
+cat << EOF > alpinestein/root/.ashrc
+export PATH="/bin:\$PATH"
+alias ll="ls -la"
+alias apkli="apk list --installed"
+EOF
+
+## set the ENV variable in .profile
+cat << EOF > alpinestein/root/.profile
+export ENV=\$HOME/.ashrc
+EOF
+
+## create the etc directory in the Alpine environment
+mkdir -p alpinestein/etc
+
+## copy the DNS resolver configuration from host to mfs
+cp /etc/resolv.conf alpinestein/etc/resolv.conf
+
+## create a symlink for apk so that we can access it directly. 
+ln -s /sbin/apk alpinestein/bin/apk
+## this is where it becomes interesting
+
+## now start a shell using chroot
+chroot alpinestein /bin/ash -c "source /root/.profile; exec /bin/ash"
