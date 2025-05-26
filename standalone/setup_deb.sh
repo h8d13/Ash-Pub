cf2e0c542fbee690a978f482fc7d4b23487604cf
HADEON <52324046+h8d13@users.noreply.github.com>
2025-05-26 08:40:10 +0200

    Update setup_deb.sh

--- standalone/setup_deb.sh
+++ standalone/setup_deb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## Script made for Rasp4+ (preferably on NVMe SSD if you want a responsive system) 
-# Or really any deb base using bash shell?
+# Or really any deb base using bash shell? Meant to be run elevated.
 # Get image from Raspi imager, Pick Armbian64 > Kde neon
 # Make sure you created a user, pws, etc
-# Update upgrade > Then run this script as root "su" to get to root. 
+
 TARGET_USER=hadean
 
 echo "Checking user..." 
